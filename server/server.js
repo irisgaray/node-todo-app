3f0526ede34f563a17436d1b86569421e24d5b85
irishua <irishua@br.ibm.com>
2018-04-02 14:47:08 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -33,6 +34,31 @@ app.get('/todos', (req, res) => {
         .catch(err => {
             res.status(400).send(err);
         });
+});
+
+// GET /todos/id:1234
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+//   res.send(req.params);  
+  
+  // Validate id using isValid
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id)
+    .then(todo => {
+        if(!todo) {
+            return res.status(404).send('Unable to find Todo document');
+            // return console.log(`Unable to find Todo document`);
+        }
+        res.status(200).send(JSON.stringify(todo, undefined, 2));
+        console.log(`${todo}`);
+    })
+    .catch(error => {
+        console.log(`Unexpected error > ${error}`);
+    });
 });
 
 
